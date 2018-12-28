c9cb35511632ebb75e0fce708871a6a0bf1eac37
Ryan Gomoto <ryan@cyrusbio.com>
2018-12-28 05:15:24 -0800

    Condense

--- scripts/save-schema.ts
+++ scripts/save-schema.ts
@@ -1,22 +1,19 @@
-import { AppSync }  from 'aws-sdk';
 import * as fs from 'fs';
 import * as path from 'path';
 import { sleep } from './sleep';
 import { configuredAppSync as appsync } from './aws-appsync';
 
-const startSchemaCreationRequest: AppSync.StartSchemaCreationRequest = {
-  apiId: process.env.APPSYNC_API_ID,
-  definition: fs.readFileSync(path.resolve(__dirname, '../src/schema.graphql')),
-};
-
-const getSchemaCreationStatusRequest: AppSync.GetSchemaCreationStatusRequest = {
-  apiId: process.env.APPSYNC_API_ID,
-};
-
 async function main() {
-  await appsync.startSchemaCreation(startSchemaCreationRequest);
+  // Save schema
+  await appsync.startSchemaCreation({
+    apiId: process.env.APPSYNC_API_ID,
+    definition: fs.readFileSync(path.resolve(__dirname, '../src/schema.graphql')),
+  });
+  // Wait for schema creation to be SUCCESS or FAILED
   while (true) {
-    const getSchemaCreationStatusResponse = await appsync.getSchemaCreationStatus(getSchemaCreationStatusRequest);
+    const getSchemaCreationStatusResponse = await appsync.getSchemaCreationStatus({
+      apiId: process.env.APPSYNC_API_ID,
+    });
     if (getSchemaCreationStatusResponse.status !== 'PROCESSING') {
       console.log(getSchemaCreationStatusResponse);
       break;
