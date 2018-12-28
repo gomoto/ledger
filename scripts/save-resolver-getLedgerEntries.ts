a27580bbbc7c4e2343a9c562828d6cf51c91f35e
Ryan Gomoto <ryan@cyrusbio.com>
2018-12-28 05:25:33 -0800

    Factor out save-resolver function

--- scripts/save-resolver-getLedgerEntries.ts
+++ scripts/save-resolver-getLedgerEntries.ts
@@ -1,46 +1,22 @@
-// Create or update resolver
-
-import { AppSync }  from 'aws-sdk';
 import * as fs from 'fs';
 import * as path from 'path';
-import { appsync } from './aws-appsync';
+import { saveResolver } from './save-resolver';
 
 async function main() {
-  // First, does resolver already exist?
-  let doesResolverExist: boolean;
-  try {
-    const getResolverRequest = {
-      apiId: process.env.APPSYNC_API_ID,
-      typeName: 'Query',
-      fieldName: 'getLedgerEntries',
-    };
-    await appsync.getResolver(getResolverRequest);
-    doesResolverExist = true;
-  } catch (e) {
-    doesResolverExist = false;
-  }
-  const createOrUpdateResolverRequest = {
-    apiId: process.env.APPSYNC_API_ID,
+  const response = await saveResolver({
     typeName: 'Query',
     fieldName: 'getLedgerEntries',
     dataSourceName: 'LedgerEntryTable',
     requestMappingTemplate: fs.readFileSync(path.resolve(__dirname, '../src/getLedgerEntries/request-mapping.vm')).toString('utf8'),
     responseMappingTemplate: fs.readFileSync(path.resolve(__dirname, '../src/getLedgerEntries/response-mapping.vm')).toString('utf8'),
-    kind: 'UNIT',
-  };
-  let response: AppSync.CreateResolverResponse | AppSync.UpdateResolverResponse;
-  if (doesResolverExist) {
-    response = await appsync.updateResolver(createOrUpdateResolverRequest);
-  } else {
-    response = await appsync.createResolver(createOrUpdateResolverRequest);
-  }
-  console.log('Saved resolver');
+  });
+  console.log('Saved resolver for getLedgerEntries');
   console.log(response);
 }
 
 main()
 .catch((error) => {
-  console.error('Failed to save resolver');
+  console.error('Failed to save resolver for getLedgerEntries');
   console.error(error);
   process.exit(1);
 })
