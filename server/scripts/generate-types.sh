6d6a0612a4b3508369bb4e11a8615c7e4e3e34f8
Ryan Gomoto <ryan@cyrusbio.com>
2019-01-13 17:02:40 -0800

    Build types into src dir

--- server/scripts/generate-types.sh
+++ server/scripts/generate-types.sh
@@ -2,7 +2,7 @@
 
 # Run this as npm script so that npm binaries are available
 graphql-schema-typescript generate-ts src2/schema.graphql \
-  --output graphql-types.d.ts \
+  --output src2/graphql-types.d.ts \
   --typePrefix '' \
   --strictNulls \
   --smartTResult \
